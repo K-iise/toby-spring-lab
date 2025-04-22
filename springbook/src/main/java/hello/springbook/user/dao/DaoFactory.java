fccb028ea458c7c29b054984dd925ae6e403837d
K-iise <91732817+K-iise@users.noreply.github.com>
2025-04-22 16:01:41 +0900

    스프링 수정.

---
+++ springbook/src/main/java/hello/springbook/user/dao/DaoFactory.java
@@ -0,0 +1,11 @@
+package hello.springbook.user.dao;
+
+public class DaoFactory {
+    public UserDao userDao(){
+        return new UserDao(connectionMaker());
+    }
+
+    public ConnectionMaker connectionMaker(){
+        return new DconnectionMaker();
+    }
+}
