dfd34676d97276edecd33260f1698a153dfc58a5
K-iise <91732817+K-iise@users.noreply.github.com>
2025-04-26 15:40:13 +0900

    2.4 스프링 테스트 적용

--- springbook/src/test/java/hello/springbook/dao/UserDaoTest.java
+++ springbook/src/test/java/hello/springbook/dao/UserDaoTest.java
@@ -3,6 +3,7 @@ package hello.springbook.dao;
 import hello.springbook.user.dao.UserDao;
 import hello.springbook.user.domain.User;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,15 +11,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(locations = "/applicationContext.xml")
+@ContextConfiguration(locations = "/test-applicationContext.xml")
 public class UserDaoTest {
     
 
@@ -28,6 +31,7 @@ public class UserDaoTest {
     private User user1;
     private User user2;
     private User user3;
+
 
     @BeforeEach
     void setUp() {
