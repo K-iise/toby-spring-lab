a504b1232e7414af00a185b9e81a90663f84b15a
K-iise <91732817+K-iise@users.noreply.github.com>
2025-04-28 19:19:39 +0900

    3.3 JDBC 전략 패턴의 최적화

--- springbook/src/main/java/hello/springbook/user/dao/UserDao.java
+++ springbook/src/main/java/hello/springbook/user/dao/UserDao.java
@@ -14,38 +14,21 @@ public class UserDao {
         this.dataSource = dataSource;
     }
 
-    public void add(User user) throws SQLException{
-        Connection c = null;
-        PreparedStatement ps = null;
-        try {
-            c = dataSource.getConnection();
+    public void add(final User user) throws SQLException {
 
-            ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-            ps.setString(1, user.getId());
-            ps.setString(2, user.getName());
-            ps.setString(3, user.getPassword());
+        // 익명 내부 클래스를 적용.
+        StatementStrategy st = new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+                ps.setString(1, user.getId());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getPassword());
 
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (ps != null){
-                try {
-                    ps.close();
-                }
-                catch (SQLException e){
-                }
+                return ps;
             }
-
-            if (c != null){
-                try {
-                    c.close();
-                } catch (SQLException e){
-
-                }
-            }
-        }
-
+        };
+        jdbcContextWithStatementStrategy(st);
     }
 
     public User get(String id) throws SQLException {
